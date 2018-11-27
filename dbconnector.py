576f3c638887b21f1f2c5136186364a60352b363
jonathan <jcclark43@gmail.com>
2018-11-27 11:45:43 -0500

    registration....round 2

--- dbconnector.py
+++ dbconnector.py
@@ -1,13 +1,15 @@
 import pymysql
 from flask import Flask, render_template, request, redirect, url_for, make_response, flash
+from flask_hashing import Hashing
 
 app = Flask(__name__)
-
-def execute_query(query):
-    db = pymysql.connect(host='academic-mysql.cc.gatech.edu', port=3306,
+hashing = Hashing(app)
+db = pymysql.connect(host='academic-mysql.cc.gatech.edu', port=3306,
                          user='cs4400_group14', passwd='AMEb4bEi', db='cs4400_group14',
                          cursorclass=pymysql.cursors.DictCursor)
-    cursor = db.cursor()
+cursor = db.cursor()
+
+def execute_query(query):
     cursor.execute(query)
     return cursor.fetchall()
 
@@ -74,24 +76,53 @@ def register_result():
         attempted_email = request.form['email']
         attempted_pwd = request.form['password']
         attempted_pwd2 = request.form['confirmPassword']
-        #CHECK IF USERNAME AND EMAIL UNIQUE
-        query_result = execute_query("select * from users where username='" + attempted_username + "'")
-        if bool(query_result) == True: #empty dictionaries evaluate to False
-            error = concat_errors(error, 'username already exists')
-        query_result = execute_query("select * from users where email='" + attempted_email + "'")
-        if bool(query_result) == True:  # empty dictionaries evaluate to False
-            error = concat_errors(error, 'email already exists')
-        #PASSWORD CHECKS
-        if len(attempted_pwd) < 8:
-            error = concat_errors(error, 'password is fewer than 8 characters')
-        if attempted_pwd != attempted_pwd2:
-            error = concat_errors(error, 'passwords do not match')
-
+        error = register_helper(attempted_username,
+                                attempted_email,
+                                attempted_pwd,
+                                attempted_pwd2)
         if error == '':
+            #INSERT USER
+            query = "INSERT INTO users(username, email, pwd) values('" + attempted_username + "','" + attempted_email + "','" + attempted_pwd + "')"
+            print query
+            query_result = execute_query(query)
+            app.logger.info(query_result)
+            '''
+            # REGISTER STAFF
+            if request.form['action'] == 'Register Staff':
+
+            # REGISTER VISITOR
+            if request.form['action'] == 'Register Visitor':
+            '''
             #also need to insert into database here, remember to hash the password, also create this same value in either staff or visitors as well
             #also need to set cookie and log them in
             return redirect(url_for('exhibits'))
     return render_template('register.html', error=error)
+
+def register_helper(username, email, pwd, pwd2):
+    error = ''
+    # CHECK IF USERNAME AND EMAIL UNIQUE
+    query_result = execute_query("select * from users where username='" + username + "'")
+    if bool(query_result) == True:  # empty dictionaries evaluate to False
+        error = concat_errors(error, 'username already exists')
+    query_result = execute_query("select * from users where email='" + email + "'")
+    if bool(query_result) == True:  # empty dictionaries evaluate to False
+        error = concat_errors(error, 'email already exists')
+    # PASSWORD CHECKS
+    if len(pwd) < 8:
+        error = concat_errors(error, 'password is fewer than 8 characters')
+    if pwd != pwd2:
+        error = concat_errors(error, 'passwords do not match')
+    '''
+            h = hashing.hash_value('secretdata', salt='abcd')
+            if hashing.check_value(h, 'secretdata', salt='abcd'):
+                error = "hash value checks out"
+            else:
+                error = "hash doesn't match"
+    '''
+    return error
+
+
+
 
 @app.route("/exhibits", methods=['GET', 'POST'])
 def exhibits():
